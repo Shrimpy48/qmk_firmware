62ddb82c9035a2df6b5dcf12114aab41da1429bc
Shrimpy48 <45856863+Shrimpy48@users.noreply.github.com>
2023-11-23 20:00:37 +0000

    Use UK keycode names

--- keyboards/crkbd/rev1/keymaps/Shrimpy48/keymap.c
+++ keyboards/crkbd/rev1/keymaps/Shrimpy48/keymap.c
@@ -1,6 +1,7 @@
 #include QMK_KEYBOARD_H
 
-#include "transactions.h"
+// #include "transactions.h"
+#include "keymap_uk.h"
 #include "keymap_steno.h"
 
 // #include "oneshot.h"
@@ -28,45 +29,45 @@ enum layers {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	[DEF] = LAYOUT_split_3x6_3(
-            KC_NO  , KC_Q   , KC_W         , KC_F       , KC_P        , KC_B    ,    KC_J   , KC_L     , KC_U       , KC_Y   , KC_QUOT, KC_NO ,
-            KC_ESC , KC_A   , KC_R         , KC_S       , KC_T        , KC_G    ,    KC_M   , KC_N     , KC_E       , KC_I   , KC_O   , KC_BSPC,
-            KC_TAB , KC_Z   , KC_X         , KC_C       , KC_D        , KC_V    ,    KC_K   , KC_H     , KC_COMM    , KC_DOT , KC_SCLN, KC_ENT ,
-                                             KC_NO      , MO(SYM)     , KC_SPC  ,    KC_RSFT, MO(NUM)  , KC_NO 
+            KC_NO  , KC_Q   , KC_W   , KC_F   , KC_P   , KC_B    ,    KC_J   , KC_L   , KC_U       , KC_Y        , KC_QUOT, KC_NO ,
+            KC_ESC , KC_A   , KC_R   , KC_S   , KC_T   , KC_G    ,    KC_M   , KC_N   , KC_E       , KC_I        , KC_O   , KC_BSPC,
+            KC_TAB , KC_Z   , KC_X   , KC_C   , KC_D   , KC_V    ,    KC_K   , KC_H   , KC_COMM    , KC_DOT      , KC_SCLN, KC_ENT ,
+                                       KC_WBAK, MO(SYM), KC_SPC  ,    KC_RSFT, MO(NUM), KC_WFWD
             ),
 	[SYM] = LAYOUT_split_3x6_3(
-            KC_NO  , KC_ESC , KC_LBRC      , KC_LCBR    , KC_LPRN     , KC_PIPE ,    KC_CIRC, KC_RPRN  , KC_RCBR    , KC_RBRC, KC_BSPC, KC_NO  ,
-            KC_NO  , KC_PLUS, KC_AMPR      , KC_QUES    , KC_EXLM     , KC_DLR  ,    KC_NUHS, KC_UNDS  , KC_EQL     , KC_ASTR, KC_MINS, KC_NO  ,
-            KC_NO  , KC_TAB , LSFT(KC_NUBS), KC_DQUO    , KC_SLSH     , KC_PERC ,    KC_HASH, KC_NUBS  , KC_COLN    , KC_GRV , KC_ENT , KC_NO  ,
-                                             KC_TRNS    , KC_TRNS     , KC_TRNS ,    KC_TRNS, KC_TRNS  , KC_TRNS
+            KC_TRNS, KC_ESC , KC_LBRC, KC_LCBR, KC_LPRN, UK_TILD ,    KC_CIRC, KC_RPRN, KC_RCBR    , KC_RBRC     , KC_BSPC, KC_TRNS,
+            KC_TRNS, KC_PLUS, KC_AMPR, KC_QUES, KC_EXLM, KC_DLR  ,    UK_HASH, KC_UNDS, KC_EQL     , KC_ASTR     , KC_MINS, KC_TRNS,
+            KC_TRNS, KC_TAB , UK_PIPE, UK_AT  , KC_SLSH, KC_PERC ,    UK_PND , UK_BSLS, KC_COLN    , KC_GRV      , KC_ENT , KC_TRNS,
+                                       KC_TRNS, KC_TRNS, KC_TRNS ,    KC_TRNS, KC_TRNS, KC_TRNS
             ),
 	[NUM] = LAYOUT_split_3x6_3(
-            KC_NO  , KC_NO  , KC_7         , KC_8       , KC_9        , KC_ESC  ,    KC_BSPC, KC_NO    , KC_NO      , KC_NO  , KC_NO  , KC_NO  ,
-            KC_NO  , KC_NO  , KC_4         , KC_5       , KC_6        , KC_DOT  ,    KC_NO  , KC_LEFT  , KC_DOWN    , KC_UP  , KC_RGHT, KC_NO  ,
-            KC_NO  , KC_0   , KC_1         , KC_2       , KC_3        , KC_TAB  ,    KC_ENT , KC_HOME  , KC_PGDN    , KC_PGUP, KC_END , KC_NO  ,
-                                             KC_TRNS    , KC_TRNS     , KC_TRNS ,    KC_TRNS, KC_TRNS  , KC_TRNS
+            KC_TRNS, KC_NO  , KC_7   , KC_8   , KC_9   , KC_ESC  ,    KC_BSPC, KC_NO  , RCS(KC_TAB), RCTL(KC_TAB), KC_NO  , KC_TRNS,
+            KC_TRNS, KC_NO  , KC_4   , KC_5   , KC_6   , KC_DOT  ,    KC_NO  , KC_LEFT, KC_DOWN    , KC_UP       , KC_RGHT, KC_TRNS,
+            KC_TRNS, KC_0   , KC_1   , KC_2   , KC_3   , KC_TAB  ,    KC_ENT , KC_HOME, KC_PGDN    , KC_PGUP     , KC_END , KC_TRNS,
+                                       KC_TRNS, KC_TRNS, KC_TRNS ,    KC_TRNS, KC_TRNS, KC_TRNS
             ),
 	[FUN] = LAYOUT_split_3x6_3(
-            KC_F1  , KC_F2  , KC_F3        , KC_F4      , KC_F5       , KC_F6   ,    KC_F7  , KC_F8    , KC_F9      , KC_F10 , KC_F11 , KC_F12 ,
-            RGB_SPI, RGB_SAI, RGB_HUI      , RGB_VAI    , RGB_TOG     , RGB_MOD ,    KC_MNXT, KC_MPLY  , KC_VOLU    , KC_PSCR, KC_DEL , KC_BRIU,
-            RGB_SPD, RGB_SAD, RGB_HUD      , RGB_VAD    , RGB_M_R     , RGB_RMOD,    KC_MPRV, KC_MUTE  , KC_VOLD    , KC_APP , KC_INS , KC_BRID,
-                                             TG(STN)    , KC_TRNS     , KC_TRNS ,    KC_TRNS, KC_TRNS  , TG(GMR)
+            KC_F1  , KC_F2  , KC_F3  , KC_F4  , KC_F5  , KC_F6   ,    KC_F7  , KC_F8  , KC_F9      , KC_F10      , KC_F11 , KC_F12 ,
+            RGB_SPI, RGB_SAI, RGB_HUI, RGB_VAI, RGB_TOG, RGB_MOD ,    KC_MNXT, KC_MPLY, KC_VOLU    , KC_PSCR     , KC_DEL , KC_BRIU,
+            RGB_SPD, RGB_SAD, RGB_HUD, RGB_VAD, RGB_M_R, RGB_RMOD,    KC_MPRV, KC_MUTE, KC_VOLD    , KC_APP      , KC_INS , KC_BRID,
+                                       TG(STN), KC_TRNS, KC_TRNS ,    KC_TRNS, KC_TRNS, TG(GMR)
             ),
 	[GMR] = LAYOUT_split_3x6_3(
-            KC_TAB , KC_Q   , KC_W         , KC_E       , KC_R        , KC_T    ,    KC_Y   , KC_U     , KC_I       , KC_O   , KC_P   , KC_ESC ,
-            KC_LSFT, KC_A   , KC_S         , KC_D       , KC_F        , KC_G    ,    KC_H   , KC_J     , KC_K       , KC_L   , KC_SCLN, KC_QUOT,
-            KC_LCTL, KC_Z   , KC_X         , KC_C       , KC_V        , KC_B    ,    KC_N   , KC_M     , KC_COMM    , KC_DOT , KC_SLSH, KC_BSPC,
-                                             KC_LALT    , KC_SPC      , KC_LBRC ,    KC_ENT , KC_RBRC  , TG(GMR)
+            KC_TAB , KC_Q   , KC_W   , KC_E   , KC_R   , KC_T    ,    KC_Y   , KC_U   , KC_I       , KC_O        , KC_P   , KC_ESC ,
+            KC_LSFT, KC_A   , KC_S   , KC_D   , KC_F   , KC_G    ,    KC_H   , KC_J   , KC_K       , KC_L        , KC_SCLN, KC_QUOT,
+            KC_LCTL, KC_Z   , KC_X   , KC_C   , KC_V   , KC_B    ,    KC_N   , KC_M   , KC_COMM    , KC_DOT      , KC_SLSH, KC_BSPC,
+                                       KC_LALT, KC_SPC , KC_LBRC ,    KC_ENT , KC_RBRC, TG(GMR)
             ),
 	[STN] = LAYOUT_split_3x6_3(
-            STN_N1 , STN_N2 , STN_N3       , STN_N4     , STN_N5      , STN_N6  ,    STN_N7 , STN_N8   , STN_N9     , STN_NA , STN_NB , STN_NC ,
-            STN_FN , STN_S1 , STN_TL       , STN_PL     , STN_HL      , STN_ST1 ,    STN_ST3, STN_FR   , STN_PR     , STN_LR , STN_TR , STN_DR ,
-            KC_NO  , STN_S2 , STN_KL       , STN_WL     , STN_RL      , STN_ST2 ,    STN_ST4, STN_RR   , STN_BR     , STN_GR , STN_SR , STN_ZR ,
-                                             TG(STN)    , STN_A       , STN_O   ,    STN_E  , STN_U    , STN_PWR
+            STN_N1 , STN_N2 , STN_N3 , STN_N4 , STN_N5 , STN_N6  ,    STN_N7 , STN_N8 , STN_N9     , STN_NA      , STN_NB , STN_NC ,
+            STN_FN , STN_S1 , STN_TL , STN_PL , STN_HL , STN_ST1 ,    STN_ST3, STN_FR , STN_PR     , STN_LR      , STN_TR , STN_DR ,
+            KC_NO  , STN_S2 , STN_KL , STN_WL , STN_RL , STN_ST2 ,    STN_ST4, STN_RR , STN_BR     , STN_GR      , STN_SR , STN_ZR ,
+                                       TG(STN), STN_A  , STN_O   ,    STN_E  , STN_U  , STN_PWR
             )
 };
 
 // shift-' = " like on US layout
-const key_override_t quote_key_override = ko_make_basic(MOD_MASK_SHIFT, KC_QUOT, LSFT(KC_AT));
+const key_override_t quote_key_override = ko_make_basic(MOD_MASK_SHIFT, UK_QUOT, UK_DQUO);
 
 // This globally defines all key overrides to be used
 const key_override_t **key_overrides = (const key_override_t *[]){
@@ -95,7 +96,7 @@ combo_t key_combos[] = {
 };
 
 layer_state_t layer_state_set_user(layer_state_t state) {
-    // Disable combos on steno and gamer layers
+    // Disable combos on alternate layers
     switch (get_highest_layer(state)) {
     case STN:
     case GMR:
@@ -261,9 +262,6 @@ bool rgb_matrix_indicators_advanced_user(uint8_t led_min, uint8_t led_max) {
     HSV hsv_yellow = {HSV_YELLOW};
     hsv_yellow.v = rgb_matrix_get_val();
     RGB rgb_yellow = hsv_to_rgb(hsv_yellow);
-    HSV hsv_red = {HSV_RED};
-    hsv_red.v = rgb_matrix_get_val();
-    RGB rgb_red = hsv_to_rgb(hsv_red);
 
     if (IS_LAYER_ON_STATE(layer_state, GMR)) {
         for (uint8_t row = 0; row < MATRIX_ROWS; ++row) {
@@ -302,7 +300,6 @@ bool rgb_matrix_indicators_advanced_user(uint8_t led_min, uint8_t led_max) {
                         rgb_matrix_set_color(index, rgb_yellow.r, rgb_yellow.g, rgb_yellow.b);
                     } else if (keycode >= STN__MIN && keycode <= STN__MAX) {
                         // Other steno keys
-                        rgb_matrix_set_color(index, rgb_red.r, rgb_red.g, rgb_red.b);
                     }
                 }
             }
