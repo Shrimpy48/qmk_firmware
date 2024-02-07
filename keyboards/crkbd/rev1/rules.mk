e715d0ee2f88d75904df9caa92b913db36ce53e0
Shrimpy48 <45856863+Shrimpy48@users.noreply.github.com>
2024-02-07 22:03:32 +0000

    Update stuff for MB pro-micro

--- keyboards/crkbd/rev1/rules.mk
+++ keyboards/crkbd/rev1/rules.mk
@@ -1,1 +1,6 @@
-CONVERT_TO=promicro_rp2040
+# Build for Mechboards RP2040 Pro-Micro
+CONVERT_TO=rp2040_ce
+
+# Emulate EEPROM using the program memory
+EEPROM_DRIVER=wear_leveling
+WEAR_LEVELING_DRIVER=rp2040_flash
